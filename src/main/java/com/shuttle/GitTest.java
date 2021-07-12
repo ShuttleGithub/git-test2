864a597448790e98ee9e9d4699fe4262587247a1
shuttle <shuttle@qq.com>
2021-07-12 17:05:54 +0800

    hot-fix commit branch

--- src/main/java/com/shuttle/GitTest.java
+++ src/main/java/com/shuttle/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,new code!");
         System.out.println("newCode3");
+        System.out.println("new branch");
     }
 }
